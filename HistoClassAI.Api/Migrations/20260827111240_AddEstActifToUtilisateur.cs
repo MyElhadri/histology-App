b97f39e8e7be2f1852c8179bd30b5765a88c37df
yassine <mohamedyassineelhadri01@gmail.com>
2026-08-27 12:13:34 +0100

    Add EstActif to Utilisateur in database via EF migration

---
+++ HistoClassAI.Api/Migrations/20260827111240_AddEstActifToUtilisateur.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HistoClassAI.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEstActifToUtilisateur : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EstActif",
+                table: "Utilisateurs",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Utilisateurs",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "EstActif",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstActif",
+                table: "Utilisateurs");
+        }
+    }
+}
